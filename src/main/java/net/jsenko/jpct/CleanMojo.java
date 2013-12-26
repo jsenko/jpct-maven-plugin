b883d3c5372f3b14e0dde7e8ea5b3eb7546feb70
Jakub Senko <jsenko@redhat.com>
2013-12-26 23:28:34 +0100

    Implemented cleanmojo.

--- src/main/java/net/jsenko/jpct/CleanMojo.java
+++ src/main/java/net/jsenko/jpct/CleanMojo.java
@@ -12,9 +12,12 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-*/
+ */
 package net.jsenko.jpct;
 
+import net.jsenko.jpct.jenkins.client.JenkinsClient;
+import net.jsenko.jpct.jenkins.client.Job;
+import net.jsenko.jpct.jenkins.client.impl.JenkinsClientFactory;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
@@ -22,9 +25,12 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
-import java.net.URL;
+import java.util.Objects;
 
 /**
+ * Mojo that deletes all Jenkins jobs that have been created by this plugin. This is verified by the presence of local job file
+ * in dataDir. If these files are deleted by 'mvn clean' for example before this mojo is executed, those jobs must be deleted
+ * manually.
  * 
  * @author Jakub Senko
  */
@@ -33,49 +39,49 @@ public class CleanMojo extends AbstractMojo
 {
 
     @Parameter(property = "basedir", required = true)
-    private File   basedir;
+    private File basedir;
 
     /**
      * Place to store plugin data (e.g. generated config).
      */
-    @Parameter(property = "dataDir", defaultValue = "${project.build.directory}/jmp", required = true)
+    @Parameter(property = "dataDir", defaultValue = "${project.build.directory}/jpct",
+            required = true)
     private File dataDir;
 
-    /**
-     * If set, delete all Jenkins jobs that have been created by this plugin.
-     */
-    @Parameter(property = "jenkinsUrl", required = false)
-    private URL jenkinsUrl;
-    
-    
-    @Parameter(property = "jenkinsUser", required = false)
-    private String jenkinsUser;
-
-    @Parameter(property = "jenkinsToken", required = false)
-    private String jenkinsToken;
-
     private Log log;
-    
+
     public void execute() throws MojoExecutionException
     {
         log = getLog();
-        try
-        {
-            // if jenkinsUrl is set {
-            // get config object
-            // get stored job names
-            // check they exist in jenkins else log warning
-            // to be sure ask if there is more that 3 jobs
-            // verify that they have been created by this plugin so we don't fuck up
-            // delete them
-            // update configuration (necessary? if deleting anyway, yes in case delete fails) }
-            // delete data dir
-            deleteRecursively(dataDir);
+
+        for (Config config : Config.getConfigForAllJobs(dataDir, log)) {
+            String jobName = config.getJobDir().getName(); // TODO improve
+            log.info("Deleting '" + jobName + "'.");
+            String savedUrl = config.get("jenkinsUrl");
+            String savedUser = config.get("jenkinsUser");
+            String savedToken = config.get("jenkinsToken");
+            JenkinsClient client = null;
+            if (savedUrl != null) {
+                if (savedUser == null || savedToken == null)
+                    client = JenkinsClientFactory.createClient(savedUrl, log);
+                else
+                    client = JenkinsClientFactory
+                            .createClient(savedUrl, savedUser, savedToken, log);
+            }
+            if (client == null) {
+                log.warn("Could not retrieve Jenkins job for '" + jobName + "'.");
+                continue;
+            }
+            Job job = client.getJobByName(jobName);
+            if (job == null) {
+                log.warn("Could not retrieve Jenkins job for '" + jobName + "'.");
+                continue;
+            }
+            deleteRecursively(config.getJobDir());
+            if (job.delete())
+                log.info("Successfully deleted '" + jobName + "'.");
         }
-        catch(Exception e)
-        {
-            throw new MojoExecutionException(e.getMessage(), e);
-        }
+
     }
 
     /**
@@ -83,8 +89,9 @@ public class CleanMojo extends AbstractMojo
      */
     private void deleteRecursively(File file)
     {
-        if(dataDir.isDirectory())
-            for(File subFile: file.listFiles())
+        Objects.requireNonNull(file);
+        if (file.isDirectory())
+            for (File subFile : file.listFiles())
                 deleteRecursively(subFile);
         file.delete();
     }
